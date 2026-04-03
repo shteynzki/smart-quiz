c8a4d5756faff970af64df7da5118d12e073bb36
Karo-1-4 <karolinaburakova14@gmail.com>
2026-04-03 21:45:01 +0300

    update readme

---
+++ frontend/src/types/quiz.ts
@@ -0,0 +1,11 @@
+export interface QuizAnswers {
+  room_type: string;
+  zones: string[];
+  area: number;
+  style: string;
+  budget: string;
+  name: string;
+  phone: string;
+  email: string;
+  comment: string;
+}
