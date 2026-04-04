42e9c75878992a066bf92ef65cd1cefd25fb83eb
eugeneshtein <eugeneshtein@yandex.ru>
2026-04-04 13:07:53 +0300

    add entrypoint.sh back

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+!/bin/sh
 
-# echo "=====Running migrations====="
-# bundle exec rails db:migrate
+echo "=====Running migrations====="
+bundle exec rails db:migrate
 
-# if [ "$RAILS_ENV" = "development" ] || [ -z "$RAILS_ENV" ]; then
-#   if bundle exec rails db:exists?; then
-#     echo "=====Database already seeded====="
-#   else
-#     echo "=====Seeding database====="
-#     bundle exec rails db:seed
-#   fi
-# fi
+if [ "$RAILS_ENV" = "development" ] || [ -z "$RAILS_ENV" ]; then
+  if bundle exec rails db:exists?; then
+    echo "=====Database already seeded====="
+  else
+    echo "=====Seeding database====="
+    bundle exec rails db:seed
+  fi
+fi
 rm -f /app/tmp/pids/server.pid
 exec bundle exec rails server -b 0.0.0.0 -p 3000
